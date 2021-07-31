6eea83635849469235fb58aa9761db2b36a734ef
israelhrm1995 <israel.moura@sankhya.com.br>
2021-07-31 15:09:31 -0300

    Category

---
+++ src/main/java/com/israelspring/course/services/CategoryService.java
@@ -0,0 +1,25 @@
+package com.israelspring.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.israelspring.course.entities.Category;
+import com.israelspring.course.repositories.CategoryRepository;
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
