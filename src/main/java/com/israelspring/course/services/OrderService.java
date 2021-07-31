a881325ffeec03ed6e8a19e315848191839122c3
israelhrm1995 <israel.moura@sankhya.com.br>
2021-07-31 14:17:28 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/israelspring/course/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.israelspring.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.israelspring.course.entities.Order;
+import com.israelspring.course.repositories.OrderRepository;
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
